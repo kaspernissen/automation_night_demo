97b8d4880104dc5328476b12216dc737406b0a32
Kasper Nissen <kasper.nissen@lego.com>
2016-10-20 21:50:35 +0200

    visualizer labels added

--- prom_graf_up.sh
+++ prom_graf_up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl create -f demo/prometheus-configmap-2.yaml
-kubectl create -f demo/prometheus-deployment.yaml
-kubectl create -f demo/node-exporter.yaml
-kubectl create -f demo/grafana-deployment.yaml
+kubectl create -f deployment/prometheus-configmap-2.yaml
+kubectl create -f deployment/prometheus-deployment.yaml
+kubectl create -f deployment/node-exporter.yaml
+kubectl create -f deployment/grafana-deployment.yaml
